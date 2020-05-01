2a28fe398a25a8abf01f7fb6eb07c70eac8dd087
royal19790617 <37723106@qq.com>
2020-05-01 11:16:07 +0800

    fix required mix

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -3,6 +3,7 @@ package com.jiker.keju;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.regex.Pattern;
 
 public class AppRunner {
@@ -13,7 +14,7 @@ public class AppRunner {
         System.out.println(receipt);
     }
 
-    public String readFile(File file) throws Exception{
+    private String readFile(File file) throws Exception{
         BufferedReader br = new BufferedReader(new FileReader(file));
         StringBuilder sb = new StringBuilder("");
         String s = null;
@@ -24,13 +25,13 @@ public class AppRunner {
         return sb.toString();
     }
 
-    public String getNumeric(String str) {
+    private String getNumeric(String str) {
         String regEx="[^0-9]";
         Pattern p = Pattern.compile(regEx);
         return p.matcher(str).replaceAll(" ").trim();
     }
 
-    public String getReceipt(String oneLine) {
+    private String getReceipt(String oneLine) {
         String[] str = oneLine.split("\\s+");
         int fee = new CalculateFee().calculate(Integer.valueOf(str[0]),Integer.valueOf(str[1]));
         return new StringBuffer("").append("收费").append(fee).append("元\r\n").toString();
